4c818f28b93ef7d485c55cea24eb5ba530fe9e06
Michal Lukasiewicz <michal.lukasiewicz@gmail.com>
2013-04-27 19:45:32 +0200

    Initial Commit

---
+++ harpybugs/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  harpybugs
+//
+//  Created by Michał Łukasiewicz on 4/27/13.
+//  Copyright (c) 2013 Michał Łukasiewicz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ harpybugs/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  harpybugs
+//
+//  Created by Michał Łukasiewicz on 4/27/13.
+//  Copyright (c) 2013 Michał Łukasiewicz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
